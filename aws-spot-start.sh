884e9517aa2fe62fb4ab3d886b4fc3d87a34eeba
Sam Thomas <sgpthomas@gmail.com>
2018-07-10 10:40:53 -0700

    fixed aws-spot-start.sh

--- aws-spot-start.sh
+++ aws-spot-start.sh
@@ -5,4 +5,5 @@ export D3MTIMEOUT=300
 export D3MHOST=172.31.38.133
 export D3MPORT=3000
 
-tmux -v new -d -s session './sklearn-pmlb-benchmarks/d3mstart.sh'
+cd /home/ubuntu
+tmux -v new -d -s session './sklearn-pmlb-benchmarks/d3mStart.sh'
